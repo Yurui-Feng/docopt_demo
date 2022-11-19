8701b0f458b25fc72b47b838fad9960efcbf22f0
Yurui-Feng <yurifeng@bu.edu>
2022-11-19 12:31:39 -0800

    add arg4 to python demo

--- demo.py
+++ demo.py
@@ -2,15 +2,17 @@
 # date: 2020-01-15
 
 """This script prints out docopt args.
-Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.py <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg1>             Takes any value (this is a required positional argument)
+[<arg4>]          Takes any value
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-""" 
+"""
 
 from docopt import docopt
+
 opt = docopt(__doc__)
 print(opt)
 print(type(opt))
